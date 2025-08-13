a40f10ce819cdaa11972c7f74dafa45601fdf2d5
Tarcyane <cardosotarcy6@gmail.com>
2025-08-13 11:38:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 3002
+app.use(express.json())
+const usuarios = []
+app.post('/usuarios', (req, res)=>{
+    const usuario = req.body //"pega" os dados da requisição 
+    usuarios.push(usuario) //adiciona o usuário na lista 
+    res.send("Usuário cadstro com sucesso!")
 
-app.get('/ola', (req, res)=>{
-    res.send("Olá 3°DS!") 
+})
 
+
+app.get('/usuarios', (req, res)=>{
+    res.send(usuarios) 
+
+})
+
+app.get("/ola", (req,res)=>{
+    res.send('olá 3°DS!')
 })
 
 app.listen(port,()=>{
     console.log("API rodando...") 
 } )
+
 
 
